b230a67a01461513e55f804e20f986b0aa8777cf
avelagapudi <anupamav@auction.com>
2017-02-10 12:49:40 -0800

    updated test

--- src/test/java/com.tenx.ms.retail/controller/sampleControllerTest.java
+++ src/test/java/com.tenx.ms.retail/controller/sampleControllerTest.java
@@ -3,50 +3,53 @@ package com.tenx.ms.retail;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+//import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import org.springframework.web.context.WebApplicationContext;
+//import org.springframework.web.context.WebApplicationContext;
 import com.tenx.ms.retail.controller.sampleController;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
-@WebAppConfiguration
+//@ContextConfiguration
+//@WebAppConfiguration
 public class sampleControllerTest {
 
-    @Autowired
-    private WebApplicationContext ctx;
+    //@Autowired
+    //private WebApplicationContext ctx;
 
     private MockMvc mockMvc;
 
-    @Before public void setUp() {
-        this.mockMvc = webAppContextSetup(ctx).build();
+    @Before
+    public void setUp() {
+        //this.mockMvc = webAppContextSetup(ctx).build();
+        this.mockMvc = standaloneSetup(new sampleController()).alwaysExpect(status().isOk()).build();
     }
 
     @Test
     public void helloWorld() throws Exception {
-        mockMvc.perform(get("/hello").accept(MediaType.TEXT_PLAIN))
+        this.mockMvc.perform(get("/hello").accept(MediaType.TEXT_PLAIN))
             .andExpect(status().isOk())
             .andExpect(content().contentType("text/plain;charset=ISO-8859-1"))
+            //.andExpect(content().contentType("text/plain"))
             .andExpect(content().string("Hello World!"));
     }
 
-    @Configuration
-    public static class TestConfiguration {
-
-        @Bean public sampleController sampleController() {
-            return new sampleController();
-        }
-
-    }
+//    @Configuration
+//    public static class TestConfiguration {
+//
+//        @Bean public sampleController sampleController() {
+//            return new sampleController();
+//        }
+//
+//    }
 }
